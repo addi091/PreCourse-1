34717650f9c90adcc20c82aedb5bf4f44f8fdbd1
Aditya Sinha <addi091@gmail.com>
2025-09-29 00:52:38 +0530

    remove commented main code precourse1

--- Exercise_1.java
+++ Exercise_1.java
@@ -52,10 +52,14 @@ class Stack {
 } 
   
 // Driver code 
-/*
- * class Main { public static void main(String args[]) { Stack s = new Stack();
- * s.push(10); s.push(20); s.push(30); System.out.println(s.pop() +
- * " Popped from stack");
- * 
- * } }
- */
+class Main { 
+    public static void main(String args[]) 
+    { 
+        Stack s = new Stack(); 
+        s.push(10); 
+        s.push(20); 
+        s.push(30); 
+        System.out.println(s.pop() + " Popped from stack"); 
+        
+    } 
+}
